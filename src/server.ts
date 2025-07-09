8b77254c4cf0ee7734e67dd0ed3bca7ffcc6e0c8
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-09 11:01:55 +0200

    knex

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World!" })
-})
+  response.json({ message: "Hello World! Let`s do it again" });
+});
 
-app.listen(3333, () => console.log(`Server is running on port 3333`))
+app.listen(3333, () => console.log(`Server is running on port 3333`));
