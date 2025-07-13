ba7bccfdc636aa6214468592e68bc5744347377a
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-13 16:01:45 +0200

    database

---
+++ src/database/migrations/20250713085313_add-updated-courses.ts
@@ -0,0 +1,13 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("courses", (table) => {
+    table.timestamp("update_at").defaultTo(knex.fn.now()).after("created_at");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("courses", (table) => {
+    table.dropColumn("update_at");
+  });
+}
