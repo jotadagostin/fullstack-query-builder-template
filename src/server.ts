e9bdf3d620b3f2affdbf9668fff7acc0aed7f99d
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-13 16:32:48 +0200

    setting knex

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
+import { knex } from "./database/knex";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World! Let`s do it again" });
+app.post("/courses", async (request: Request, response: Response) => {
+  const { name } = request.body;
+
+  await knex("courses").insert({ name });
+  // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
+  response.status(201).json();
+});
+
+app.get("/courses", async (request: Request, response: Response) => {
+  // const courses = await knex.raw("SELECT * FROM courses");
+  const courses = await knex("courses").select().orderBy("name", "desc");
+
+  response.json(courses);
 });
 
 app.listen(3333, () => console.log(`Server is running on port 3333`));
