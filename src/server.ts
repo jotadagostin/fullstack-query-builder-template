7a3ee566a17101e1e69f3ef9248eeec4afc6ce96
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-13 16:40:02 +0200

    new methods
    put and delete

--- src/server.ts
+++ src/server.ts
@@ -9,14 +9,29 @@ app.post("/courses", async (request: Request, response: Response) => {
 
   await knex("courses").insert({ name });
   // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
-  response.status(201).json();
+  return response.status(201).json();
 });
 
 app.get("/courses", async (request: Request, response: Response) => {
   // const courses = await knex.raw("SELECT * FROM courses");
   const courses = await knex("courses").select().orderBy("name", "desc");
 
-  response.json(courses);
+  return response.json(courses);
+});
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+
+  await knex("courses").update({ name }).where({ id });
+  return response.json();
+});
+
+app.delete("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  await knex("courses").delete().where({ id });
+  return response.json();
 });
 
 app.listen(3333, () => console.log(`Server is running on port 3333`));
